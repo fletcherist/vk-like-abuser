a471b14e4b5209cb45586a1e63565345e849c6fa
fletcherist <romanovexports@gmail.com>
2017-02-12 18:31:42 +0300

    add vk & firebase db modules

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const PEOPLE_AMOUNT = 10
+const PEOPLE_AMOUNT = 17
 const VK_API_WAIT = 1000
 
 class Console {
   constructor (msg) {
     this.config = {
       errors: true,
-      success: true,
+      success: false,
       notifications: true
     }
   }
@@ -123,11 +123,27 @@ class Likes {
   }
 }
 
-let groupsInstance = null
-class Groups extends Users {
-  constructor () {
+class Like extends Likes {
+  constructor ({object, target}) {
     super()
+    return new Promise ((resolve, reject) => {
+      setTimeout(() => {
+        if (users.findById(target) && users.findById(object)) {
+          likes.add({
+            target: target,
+            object: object
+          })
+        }
 
+        resolve()
+      }, VK_API_WAIT)
+    })
+  }
+}
+
+let groupsInstance = null
+class Groups {
+  constructor () {
     if (!groupsInstance) {
       this.groups = []
       this.peopleInGroup = 0
@@ -135,32 +151,51 @@ class Groups extends Users {
 
       groupsInstance = this
     }
-
+    this.splitIntoGroups()
     return groupsInstance
   }
 
   destructor () {
     groupsInstance = null
+    return null
+  }
+
+  splitIntoGroups () {
+    let people = new Users().getUsers()
+
+    let peopleAmount = people.length
+    this.findOptimalGroupsCount(peopleAmount)
+    people = shuffleArray(people)
+
+    for (let i = 0; i < this.groupsCount; i++) {
+      this.groups.push([])
+      for (let e = 0; e < this.peopleInGroup; e++) {
+        this.groups[i].push(people[0])
+        people.splice(0, 1)
+      }
+    }
+
+    return this.groups
   }
 
   showGroups () {
     console.log(this.groups)
   }
 
-  findOptimalGroupsCount () {
-    let peopleAmount = this.usersCount
+  findOptimalGroupsCount (peopleAmount) {
     if (!peopleAmount) return new Console().error('Failed to split into groups')
     let medianArray = []
     let count = 0
     for (let i = 2; i <= peopleAmount; i++) {
-      
       if (peopleAmount % i == 0) {
         count++
         medianArray.push(i)
       }
     }
-    console.log('count', count)
-    console.log(medianArray)
+
+    if (count === 0 || count === 1) {
+      return this.findOptimalGroupsCount(peopleAmount - 1)
+    }
 
     const peopleInGroup = medianArray[Math.floor(medianArray.length / 2)]
     const groupsCount = peopleAmount / peopleInGroup
@@ -177,6 +212,7 @@ class Groups extends Users {
 
 class Group extends Groups {
   constructor (users) {
+    console.log('eeee', users)
     super()
     if (!users || users.length === 0) return new Console().error('{Groups}: No any users have been provided')
     for (let user of users) {
@@ -198,73 +234,195 @@ class Tests {
   }
 }
 
-new Tests().generateRandomUsers()
 
 const users = new Users()
-const likes = new Likes()
+new Tests().generateRandomUsers();
 
-const addLike = ({object, target}) => {
-  return new Promise ((resolve, reject) => {
-    setTimeout(() => {
-      if (users.findById(target) && users.findById(object)) {
-        likes.add({
-          target: target,
-          object: object
-        })
-      }
-
-      resolve()
-    }, VK_API_WAIT)
-  })
-}
-
-console.log(users)
-
-let people = users.getUsers()
-people = shuffleArray(people)
+const groups = new Groups()
 
 
-const _groups = new Groups()
 
-const a = new Groups()
-console.log(a)
-
-console.log(people)
-console.log(a)
-
-const groups = []
-
-const { groupsCount, peopleInGroup } = a
-
-for (let i = 0; i < groupsCount; i++) {
-  groups.push([])
-  for (let e = 0; e < peopleInGroup; e++) {
-    groups[i].push(people[0])
-    people.splice(0, 1)
+class Engine {
+  constructor () {
+    this.users = new Users()
+    this.groups = new Groups()    
   }
 }
 
-async function doWork () {
-  for (let group of groups) {
-    console.log('-----')
 
-    for (let i = 0; i < group.length; i++) {
-      for (let e = 0; e < group.length; e++) {
-        if (i !== e) {
-          const object = group[i].id
-          const target = group[e].id
+const VKApi = require('node-vkapi')
+const ACCESS_TOKEN = 'bd7026f0b47f6ed7212ccc4f5d56f54b20d26d10c617731ddcfc7c8007b9bd5ea634806e064bf87b5a754'
+class VK {
+  constructor (access_token) {
+    if (!access_token) return new Console().error('No access token provided')
+    this.vk = new VKApi({
+      token: access_token
+    })
+  }
 
-          await addLike({object, target})
-        }
-      }
-    }
-  }  
+  getUser (user_ids) {
+    return new Promise((resolve, reject) => {
+      this.vk.call('users.get', {
+        user_ids: user_ids
+      }).then(res => {
+        console.log(res)
+        return resolve()
+      })
+      .catch(e => {
+        return reject(e)
+      })
+    })
+  }
+
+  isUserExist (user_ids) {
+    return new Promise((resolve, reject) => {
+      this.vk.call('users.get', {
+
+      })
+    })
+  }
+
+  like () {
+    return new Promise ((resolve, reject) => {
+      this.vk.call('likes.add', {
+        type: 'post',
+        owner_id: 142395293,
+        item_id: 318
+      }).then(res => {
+        console.log(res)
+        return resolve()
+      })
+      .catch(e => reject(e))
+    })
+  }
+
+  getPhotos (user_id) {
+    return new Promise ((resolve, reject) => {
+      if (!user_id) return reject('No user id provided')
+      this.vk.call('photos.get', {
+        owner_id: user_id,
+        album_id: 'profile',
+        rev: 0
+      }).then(res => {
+        console.log(res)
+        return resolve(res)
+      })
+      .catch(e => {
+        return reject(e)
+      })
+    })
+  }
 }
 
-doWork()
+const vk = new VK(ACCESS_TOKEN)
+vk.like()
+.then(r => new Console().success('Like has been set.'))
+.catch(e => new Console().error(e))
 
 
-console.log(likes.showLikes())
+const firebase = require('firebase')
+const firebaseConfig = require('./firebaseConfig')
+
+let dbInstance = null
+class DB {
+  constructor () {
+    if (!dbInstance) {
+      this.app = firebase.initializeApp(firebaseConfig)
+      this.db = this.app.database()
+
+      dbInstance = this
+    }
+
+    return dbInstance
+  } 
+
+  getUsers () {
+    return new Promise ((resolve, reject) => {
+      this.db.ref('/users').once('value')
+      .then(snapshot => {
+        console.log(snapshot.val())
+      })
+    })
+  }
+
+  isUserExist (id) {
+    return new Promise ((resolve, reject) => {
+      let user = this.db.ref(`/users/${id}`)
+      user.once('value').then(r => {
+        if (r.val() == null) {
+          return resolve()
+        }
+
+        return reject()
+      })
+    })
+  }
+
+  addUser ({username, token, id}) {
+    if (!username) return new Console().error('{DB} No username provided')
+    if (!token) return new Console().error('{DB} No access token provided')
+    if (!id) return new Console().error('{DB} No vk id provided')
+
+    this.isUserExist(id)
+    .then(isUserExist => {
+        this.db.ref(`users/${id}`).set({
+          username,
+          token,
+          id
+        })
+      }
+    ).catch(e => {
+      return new Console().error('{DB} User is already exist')
+    })
+  }
+
+  auth () {
+    // firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    //   // Handle Errors here.
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   // ...
+    // });
+  }
+}
+
+let db = new DB()
+db.addUser({
+  username: 'Leo',
+  token: 'asdasdsad',
+  id: '44'
+})
+
+
+// const users = new Users()
+// const likes = new Likes()
+
+// console.log(users)
+console.log(groups)
+// groups.findOptimalGroupsCount()
+
+
+// async function doWork () {
+//   for (let group of groups) {
+//     console.log('-----')
+
+//     for (let i = 0; i < group.length; i++) {
+//       for (let e = 0; e < group.length; e++) {
+//         if (i !== e) {
+//           const object = group[i].id
+//           const target = group[e].id
+
+//           await new Like({object, target})
+//         }
+//       }
+//     }
+//   }  
+// }
+
+// doWork()
+
+
+// console.log(likes.showLikes())
 
 
 function shuffleArray (arr) {
@@ -281,4 +439,3 @@ function shuffleArray (arr) {
   }
   return arr
 }
-
--- test.js
+++
@@ -1,9 +0,0 @@
-let a = 25
-let x = 0
-let p = 0
-while (p < 230) {
-  p += a
-  x += 3
-}
-
-console.log(x)
