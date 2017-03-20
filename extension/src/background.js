d18eebee90689b07446c19d8ec297d060981cbe7
fletcherist <romanovexports@gmail.com>
2017-03-21 00:41:15 +0300

    0.1.3

--- extension/src/background.js
+++ extension/src/background.js
@@ -8,39 +8,106 @@
   }
 
   const API = 'https://api.vk.com/method'
+  const TASKS_LATENCY = 1.5
 
-  firebase.initializeApp(config)
-  console.log('hello world')
-  console.log(chrome.tabs)
-
-  console.log(chrome.storage)
   chrome.storage.local.get(null, function (storage) {
-    const { user_id, access_token, username } = storage
-    if (access_token && user_id) {
+    const { user_id, access_token, username, latestLike } = storage
 
-      setTimeout(() => {
-        like({access_token: access_token})
-        // fetch(`${API}/users.get?access_token=${access_token}`)
-        //   .then(r => r.json())
-        //   .then(r => {
-        //     console.log(r)
-        //   })
-      }, 10000)
+    if (!latestLike) {
+      return updateLatestLike()
+    } else {
+      if (!isReadyForTask(latestLike)) {
+        return false
+      }
     }
-    console.log(storage)
+
+    firebase.initializeApp(config)
+    const db = firebase.database()
+
+    if (access_token && user_id) {
+      const tasksLink = `/tasks/${user_id}`
+      const tasks = db.ref(tasksLink)
+
+      // like({access_token: access_token})
+      getTasks()
+
+      // Listening for tasks 
+      // (0 - not done yet)
+      // (1 - done)
+      // (2 - recieved, failure while processing)
+
+      function getTasks () {
+        tasks.orderByChild('createdAt').limitToLast(1).once('value', snap => {
+          if (!snap) return false
+          let task = snap.val()
+          const taskKey = Object.keys(task)
+
+          task = task[taskKey]
+          if (task && task !== null) {
+            const { object, target, item, status } = task
+
+            if (object && target && item) {
+              like({
+                owner_id: target,
+                item_id: item,
+                access_token: access_token
+              }).then(() => {
+                db.ref(`${tasksLink}/${taskKey}/status`)
+                  .transaction(current => 1) 
+              }).catch(e => {
+                db.ref(`${tasksLink}/${taskKey}`).update({
+                  status: 2,
+                  error: e.toString()
+                })
+              })
+            }
+          }
+        })
+      }
+    }
   })
 
+
   function like (options) {
-    const {
-      owner_id = 96170043,
-      item_id = 416600058,
-      access_token
-    } = options
-    return fetch(`${API}/likes.add?type=photo&owner_id=${owner_id}&&item_id=${item_id}&access_token=${access_token}`)
-      .then(r => r.json())
-      .then(r => {
-        console.log(r)
-      })
-    
+    return new Promise((resolve, reject) => {
+      const {
+        owner_id = 96170043,
+        item_id = 416600058,
+        access_token
+      } = options
+      return fetch(`${API}/likes.add?type=photo&owner_id=${owner_id}&&item_id=${item_id}&access_token=${access_token}`)
+        .then(r => r.json())
+        .then(r => {
+          return resolve()
+        })
+        .catch(e => {
+          return reject(e)
+        })
+    })
+  }
+
+  function updateLatestLike () {
+    chrome.storage.local.set({
+      latestLike: Number(new Date())
+    }, function (storage) {})
+  }
+
+  function isReadyForTask (latestLike) {
+    if (typeof latestLike === 'object') {
+      updateLatestLike()
+      return false
+    }
+    // in ms
+    let diff = new Date() - new Date(latestLike)
+    // in s
+    diff /= 1000
+    // in m
+    diff /= 60
+
+    if (diff >= TASKS_LATENCY) {
+      return true
+    }
+
+    return false
   }
 })()
