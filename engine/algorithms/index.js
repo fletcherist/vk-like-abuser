93a5bc699d5dd0977c010e1a6ac4b10e467e4c67
fletcherist <romanovexports@gmail.com>
2017-03-26 00:48:04 +0300

    Fix terrible bug

--- engine/algorithms/index.js
+++ engine/algorithms/index.js
@@ -1,7 +1,7 @@
 const Queue = require('./queue')
 const Groups = require('./groups')
-const FastToTarget = require('./FastToTarget')
-const FastFromTarget = require('./FastFromTarget')
+const FastToTarget = require('./fastToTarget')
+const FastFromTarget = require('./fastFromTarget')
 
 const algorithms = {
   Queue,
