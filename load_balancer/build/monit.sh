ab87f91c6319d748f8432c808b6ebf7fecc8b0af
phil romanov <romanovexports@gmail.com>
2017-04-30 15:53:10 +0300

    Deploy [load_balancer]

--- load_balancer/build/monit.sh
+++ load_balancer/build/monit.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m 'Deploy [load_balancer]'
 git push
-ssh root@89.223.31.14  "PATH=/root/.nvm/versions/node/v7.9.0/bin:/root/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/root/opt/bin && cd vkabuser/load_balancer && git stash && git pull && pm2 restart load_balancer.js"
+ssh root@89.223.31.14  "PATH=/root/.nvm/versions/node/v7.9.0/bin:/root/opt/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/root/opt/bin && cd vkabuser/load_balancer && git stash && git pull && npm install && pm2 restart load_balancer.js"
